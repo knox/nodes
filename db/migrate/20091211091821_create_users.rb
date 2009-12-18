a1881f8e026031552497861eb448b54f64f8a95b
unknown <Mikolas Bingemer@.(none)>
2009-12-18 13:43:51 +0100

    Extended User model, improved seeds

--- db/migrate/20091211091821_create_users.rb
+++ db/migrate/20091211091821_create_users.rb
@@ -1,19 +1,21 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table "users", :force => true do |t|
-      t.column :login,                     :string
-      t.column :email,                     :string
+      t.column :login,                     :string, :limit => 40
+      t.column :name,                      :string, :limit => 100, :default => '', :null => true
+      t.column :email,                     :string, :limit => 100
       t.column :crypted_password,          :string, :limit => 40
       t.column :salt,                      :string, :limit => 40
       t.column :created_at,                :datetime
       t.column :updated_at,                :datetime
-      t.column :remember_token,            :string
+      t.column :remember_token,            :string, :limit => 40
       t.column :remember_token_expires_at, :datetime
       t.column :activation_code, :string, :limit => 40
       t.column :activated_at, :datetime
       t.column :password_reset_code, :string, :limit => 40
       t.column :enabled, :boolean, :default => true           
     end
+    add_index :users, :login, :unique => true
   end
   
   def self.down
--- db/schema.rb
+++ db/schema.rb
@@ -51,18 +51,21 @@ ActiveRecord::Schema.define(:version => 20091215111757) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "login"
-    t.string   "email"
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
     t.string   "crypted_password",          :limit => 40
     t.string   "salt",                      :limit => 40
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "remember_token"
+    t.string   "remember_token",            :limit => 40
     t.datetime "remember_token_expires_at"
     t.string   "activation_code",           :limit => 40
     t.datetime "activated_at"
     t.string   "password_reset_code",       :limit => 40
-    t.boolean  "enabled",                                 :default => true
+    t.boolean  "enabled",                                  :default => true
   end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
