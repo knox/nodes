bb121872e5aec6c1c0ed524fcf05e0bcc6bbb57e
Knox <mickey@netfreaks.org>
2010-01-11 14:00:43 +0100

    Redirect to own profile after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -48,8 +48,8 @@ class SessionsController < ApplicationController
     self.current_user = user
     new_cookie_flag = (params[:remember_me] == "1")
     handle_remember_cookie! new_cookie_flag
-    redirect_back_or_default('/')
     flash[:notice] = "Logged in successfully."
+    redirect_to people_path(user)
   end
 
   def failed_login(message)
